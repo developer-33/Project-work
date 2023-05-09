1179caaa8972203b5901a480f69b1dd4ec2df720
developer_33 <burton.fletcher@aol.com>
2023-05-09 14:17:50 -0500

    JavaScript

---
+++ problem-two.js
@@ -0,0 +1,16 @@
+// Oh no, it doesn't look like the fence post is holding! It seems that there are just too many zombies, and they are threatening to overwhelm your defenses. Luckily for you, the writer of these JavaScript problems doesn't want you to die, so you also miraculously have a fleet of robot turrets at your command to help repel the attack.
+// Write a function called directTurretDefenceCommand that takes in 2 parameters: The first should be called numOfTurrets (which will always be given a positive integer argument value), and the second should be called numOfDetectedZombies (which will always be given a positive integer argument value).
+// Your function should log to the console a string that matches the following format: "Directing X turrets' fire to zombie # ____." where the X represents how many turrets you have, and the blank space represents the current zombie being targeted. Your function should run as many loop iterates as there are numOfDetectedZombies.
+
+function directTurretDefenceCommand(numOfTurrets, numOfDetectedZombies){
+    for (let numOfZombies = 1; numOfZombies <= numOfDetectedZombies; numOfZombies++){
+        console,log("directing" + numOfTurrets + "turrets fire to zombie") + numOfZombies+"."){
+    
+            }
+        }
+
+
+    
+
+
+
